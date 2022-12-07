4b0d5bcf652599dc645aea6ff4a3264701da828a
fkelepce <83729090+fkelepce@users.noreply.github.com>
2022-12-07 06:49:01 +0300

    Add files via upload

--- Java101/Fibonacci/src/Main.java
+++ Java101/Fibonacci/src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
+
+    static int fib(int n) {
+        if( n==1 || n==2 ) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+
     public static void main(String[] args) {
-
-        int n1=0, n2=1 ,n3, count=10;
-
-        System.out.print(n1+" "+n2);
-
-        for(int i=2; i<count; ++i) {
-                    n3=n1+n2;
-                    System.out.print(" "+n3);
-                    n1=n2;
-                    n2=n3;
-            }
+        System.out.println(fib(20));
     }
 }
